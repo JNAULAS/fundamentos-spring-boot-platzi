8873aa1b8c57dcd821f3c7d1841c3cd2c708dae1
j.naulas <j.naulas@jardinazuayo.fin.ec>
2021-10-30 03:01:10 -0500

    Asignamos las propiedades del archivo properties a variables de clase y hacermos uso esto deltro del Bean DataSource para conectar a la base de datos.

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/configuration/GeneralConfiguration.java
@@ -8,10 +8,12 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:connection.properties")//Aqui pasamos el class path de nuestro archivo de propiedades
 /*
 Anotacion que realiza la configuracion de nuestro pojo como dependencia e ingresamos la clase a configurar
 Esta clase indica que se va a representar como propiedades dentro de la Aplicacion
@@ -19,12 +21,22 @@ Esta clase indica que se va a representar como propiedades dentro de la Aplicaci
 @EnableConfigurationProperties(UserPojo.class)
 public class GeneralConfiguration {
     //@Value + ("${nombre de la propiedad creada}")
-    @Value("${value.name}")
+    @Value("${value.name}")//Mapeamos la propiedad a una variable
     private String name;
     @Value("${value.apellido}")
     private String apellido;
     @Value("${value.random}")
     private String random;
+//Propiedades para conectar a la base de datos
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+    @Value("${driver}")
+    private String driver;
+    @Value("${username}")
+    private String username;
+    @Value("${password}")
+    private String password;
+
 
     @Bean
     public MyBeanWithProperties function() {
@@ -33,6 +45,7 @@ public class GeneralConfiguration {
     //Bean para configurar nuestra base de datos: Para construir utilizamos  DataSourceBuilder
     // DataSource es una interfaz, entonces cuando inyectemos esta dependencia nosotros ya podemos utilizar toda esta configuracion
     // Invertimos el control para que spring boot nos inyecte esta dependencia.
+    /*
     @Bean
     public DataSource dataSource(){
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
@@ -43,4 +56,16 @@ public class GeneralConfiguration {
         //Retorna la configuracion construida
         return  dataSourceBuilder.build();
     }
+    */
+    @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName(driver);//Driver
+        dataSourceBuilder.url(jdbcUrl);//Url a nivel de base de datos
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
+        //Retorna la configuracion construida
+        return  dataSourceBuilder.build();
+    }
+
 }
